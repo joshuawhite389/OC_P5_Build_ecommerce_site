06257dcf69fd238e6a7485cc8f41144c9802c368
Joshua White <56774271+joshuawhite389@users.noreply.github.com>
2024-05-05 21:49:31 -0400

    connected product pages to product api, populated pages

--- front/scripts/product.js
+++ front/scripts/product.js
@@ -1,3 +1,9 @@
+// variables
+const imgContainer = document.getElementsByClassName("item__img")[0];
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const colorPicker = document.getElementById("colors");
 
 // helper function to get product id from URL search params
 const getProductId = () => {
@@ -13,9 +19,32 @@ const getProductId = () => {
 document.addEventListener("DOMContentLoaded", async () => {
     const prodId = getProductId();
     try {
-        const response = await fetch(`http://localhost:3000/api/products/${prodId}`);
+        const response = await fetch(
+            `http://localhost:3000/api/products/${prodId}`
+        );
         const data = await response.json();
+        loadProductInfo(data);
     } catch (error) {
         console.log(error);
     }
 });
+
+// populate product page with info from product api
+const loadProductInfo = (data) => {
+    const productImg = document.createElement("img");
+    
+    data.colors.forEach(element => {
+        const colorOption = document.createElement("option");
+        colorOption.value = element;
+        colorOption.innerText = element;
+        colorPicker.appendChild(colorOption);
+    })
+
+    productImg.src = data.imageUrl;
+    productImg.alt = "Photo of a sofa";
+    title.innerText = data.name;
+    price.innerText = data.price;
+    description.innerText = data.description;
+
+    imgContainer.appendChild(productImg);
+};
