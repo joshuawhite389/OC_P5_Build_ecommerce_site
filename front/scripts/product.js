819a529ccd9d375bc43cf7ec266ac3df3e03b45a
Joshua White <56774271+joshuawhite389@users.noreply.github.com>
2024-05-06 22:59:52 -0400

    started creating object and local storage for cart

--- front/scripts/product.js
+++ front/scripts/product.js
@@ -1,10 +1,3 @@
-// variables
-const imgContainer = document.getElementsByClassName("item__img")[0];
-const title = document.getElementById("title");
-const price = document.getElementById("price");
-const description = document.getElementById("description");
-const colorPicker = document.getElementById("colors");
-
 // helper function to get product id from URL search params
 const getProductId = () => {
     const url = window.location.href;
@@ -14,6 +7,38 @@ const getProductId = () => {
     const productId = array[0];
     return productId;
 };
+
+// variables
+const imgContainer = document.getElementsByClassName("item__img")[0];
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const colorPicker = document.getElementById("colors");
+const addToCartBtn = document.getElementById("addToCart");
+const quantity = document.getElementById("quantity");
+let product = {
+    id: getProductId(),
+    quantity: "",
+    color: "",
+};
+let cart = JSON.parse(localStorage.getItem("cart"));
+
+//event listeners
+addToCartBtn.addEventListener("click", () => {
+    addProductToCart();
+});
+colorPicker.addEventListener("change", () => {
+    product = {
+        ...product,
+        color: colorPicker.value,
+    };
+});
+quantity.addEventListener("change", () => {
+    product = {
+        ...product,
+        quantity: quantity.value,
+    };
+});
 
 // fetch data from product that was selected
 document.addEventListener("DOMContentLoaded", async () => {
@@ -32,19 +57,42 @@ document.addEventListener("DOMContentLoaded", async () => {
 // populate product page with info from product api
 const loadProductInfo = (data) => {
     const productImg = document.createElement("img");
-    
-    data.colors.forEach(element => {
+
+    data.colors.forEach((element) => {
         const colorOption = document.createElement("option");
         colorOption.value = element;
         colorOption.innerText = element;
         colorPicker.appendChild(colorOption);
-    })
+    });
 
     productImg.src = data.imageUrl;
-    productImg.alt = "Photo of a sofa";
+    productImg.alt = data.altTxt;
     title.innerText = data.name;
     price.innerText = data.price;
     description.innerText = data.description;
 
     imgContainer.appendChild(productImg);
+};
+
+const addProductToCart = () => {
+    console.log(product);
+    if (!localStorage.getItem("cart")) {
+        localStorage.setItem("cart", "[]");
+    }
+    
+    
+    if(cart.length === 0) {
+        cart.push(product);
+    } else {
+        
+    }
+
+    
+    // else, check item's id and color
+    // if you can find that item in cart, += quantity of new product
+    
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    // localStorage.setItem("products", JSON.stringify(product));
+    
 };
